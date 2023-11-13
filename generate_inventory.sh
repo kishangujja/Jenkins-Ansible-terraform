cb6ade538443e0ef0a97ba5ce8342f0b17749d7a
hussain147 <hussainashrafi406@gmail.com>
2023-11-14 00:18:42 +0530

    updated

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-frontend_ip=$(terraform output -json | jq -r '.amzn_linux.value.public_ip')
-backend_ip=$(terraform output -json | jq -r '.ubuntu_linux.value.public_ip')
+# Retrieve public IPs from Terraform output without using jq
+frontend_ip=$(terraform output -json | grep -oP '"amzn_linux":\s*\{\s*"public_ip": "\K[^"]+')
+backend_ip=$(terraform output -json | grep -oP '"ubuntu_linux":\s*\{\s*"public_ip": "\K[^"]+')
 
 cat <<EOF
 [frontend]
@@ -10,3 +11,4 @@ $frontend_ip
 [backend]
 $backend_ip
 EOF
+
