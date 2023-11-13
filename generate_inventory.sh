a270a44dcdb11cfca7caf6301af7330436a08d73
hussain147 <hussainashrafi406@gmail.com>
2023-11-14 01:13:59 +0530

    updated

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Run Terraform to get the public IPs output
-amazon_linux_ip=$(terraform output amzn_linux_ip)
-ubuntu22_ip=$(terraform output ubuntu_linux_ip)
+amzn_linux_ip=$(terraform output amzn_linux_public_ip)
+ubuntu_linux_ip=$(terraform output ubuntu_linux_public_ip)
 
 # Create an INI format dynamic inventory
-inventory_content="[amazon_linux]\namzn_linux ansible_host=${amzn_linux_ip} ansible_user=amzn_linux\n\n"
-inventory_content+="[ubuntu22]\nubuntu_linux ansible_host=${ubuntu_linux_ip} ansible_user=ubuntu_linux\n"
+inventory_content="[frontend]\n${amzn_linux_ip}"
+inventory_content+="[backend]\n${ubuntu_linux_ip}"
 
 # Write the inventory to a file
 echo -e "$inventory_content" > inventory.ini
--- main.tf
+++ main.tf
@@ -21,3 +21,12 @@ resource "aws_instance" "ubuntu_linux" {
     Name = "ubuntu22"
   }
 }
+
+
+output "amzn_linux_public_ip" {
+  value = aws_instance.amzn_linux.public_ip
+}
+
+output "ubuntu_linux_public_ip" {
+  value = aws_instance.ubuntu_linux.public_ip
+}
